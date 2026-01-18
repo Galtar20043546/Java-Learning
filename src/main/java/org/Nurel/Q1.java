4956f492eab0f436fd311114933a3b160a3404fd
Nurel <nazi070582@gmail.com>
2026-01-18 21:41:16 +0600

    Фибоначи с рекурсией

---
+++ src/main/java/org/Nurel/Q1.java
@@ -0,0 +1,15 @@
+package org.Nurel;
+
+public class Q1 {
+    public static void main(String[] args) {
+        System.out.println(fiborec(6));
+    }
+
+    public static int fiborec(int n) {
+        if (n <= 2){
+            return 1;
+        } else {
+             return fiborec(n - 1) + fiborec(n - 2);
+        }
+    }
+}
---
+++ src/main/java/org/Nurel/Q2.java
@@ -0,0 +1,4 @@
+package org.Nurel;
+
+public class Q2 {
+}
