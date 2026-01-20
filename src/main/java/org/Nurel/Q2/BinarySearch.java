b37307e0e419f0fb764e503398d480508fd5e9a1
Nurel <nazi070582@gmail.com>
2026-01-21 01:00:13 +0600

    Решил

--- src/main/java/org/Nurel/Q2/BinarySearch.java
+++ src/main/java/org/Nurel/Q2/BinarySearch.java
@@ -1,4 +1,36 @@
 package org.Nurel.Q2;
 
 public class BinarySearch {
+
+    public static void main(String[] args) {
+        // int[] myArray = {};  // пустой массив.
+        // int[] myArray = {1}; // массив с одним элементом.
+        // int[] myArray = {1,4,9};   // массив с тремя элементами.
+        int[] myArray = {1,2,4,5,7,9,11,14,17,20,23}; // четное/нечетное число элементов.
+        int target = 1;
+
+        int result = biSearch(myArray,target);
+
+        System.out.println(result);
+    }
+
+    public static int biSearch(int[] array, int target){
+        int low = 0;
+        int high = array.length - 1;
+
+        while (low <= high){
+            int mid =  (low + high) / 2;
+
+            int guess = array[mid];
+            if (target == guess){
+                return mid;
+            }
+            if (guess < target){
+                low = mid + 1;
+            } else {
+                 high = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
