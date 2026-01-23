4b77381874dda3aaa99394a58a5fdb760c494a40
Nurel <nazi070582@gmail.com>
2026-01-24 00:36:03 +0600

    Решил №3 LinkedList: String, Double

--- src/main/java/org/Nurel/Q3/LinkedList/Main.java
+++ src/main/java/org/Nurel/Q3/LinkedList/Main.java
@@ -2,15 +2,26 @@ package org.Nurel.Q3.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedListInteger listInteger = new LinkedListInteger(4);
+        LinkedListDouble list = new LinkedListDouble(4);
 
-        listInteger.add(2);
-        listInteger.add(4);
-        listInteger.add(5);
-        listInteger.add(7);
-        System.out.println("Size: " + listInteger.size());
+        // list.add(2);
+        // list.add(4);
+        // list.add(5);
+        // list.add(7);
 
-        System.out.println("Get: " + listInteger.get(1));
+        // list.add("Nurel");
+        // list.add("Volve");
+        // list.add("Mancini");
+        // list.add("Pep");
+
+        list.add(2.4);
+        list.add(14.6);
+        list.add(135.7);
+        list.add(0.5252);
+
+        System.out.println("Size: " + list.size());
+
+        System.out.println("Get: " + list.get(1));
 
         // "удаление элемента по заданному индексу" - Не смог решить.
     }
