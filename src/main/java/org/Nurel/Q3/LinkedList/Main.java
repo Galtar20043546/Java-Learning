6b7a6e6b3edb7a52296142e06e9044391e6e1a58
Nurel <nazi070582@gmail.com>
2026-01-24 00:20:01 +0600

    Решил №3 LinkedList: Integer

--- src/main/java/org/Nurel/Q3/LinkedList/Main.java
+++ src/main/java/org/Nurel/Q3/LinkedList/Main.java
@@ -1,4 +1,17 @@
 package org.Nurel.Q3.LinkedList;
 
 public class Main {
+    public static void main(String[] args) {
+        LinkedListInteger listInteger = new LinkedListInteger(4);
+
+        listInteger.add(2);
+        listInteger.add(4);
+        listInteger.add(5);
+        listInteger.add(7);
+        System.out.println("Size: " + listInteger.size());
+
+        System.out.println("Get: " + listInteger.get(1));
+
+        // "удаление элемента по заданному индексу" - Не смог решить.
+    }
 }
